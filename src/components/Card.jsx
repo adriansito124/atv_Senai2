96bbaed2e3e8ce23fd9bb4fbffbaa527bf90125d
Adrian Gobara <adriangobara@gmail.com>
2024-09-06 14:47:52 -0300

    bolinha finalmente funcionou

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/prop-types */
+import style from './Card.module.css'
+
 export const Card = (props) => {
+  
   return(
-      <div>
+      <div className={style.corFundo}>
+        <div className={style.alinhaa}>
           <h1>{props.name}</h1>
+          <h1>-</h1>
           <h2>{props.desc}</h2>
-          <p>{props.value}</p>
+        </div>
           <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+
+          <div className={style.espasso}>
+              <div className={style.alinhaa}>
+                  <p>Preço: </p>
+                  <p>R$ {props.value}</p>
+              </div>
+              <div className={props.status ? style.bolinha : style.vermelho }>
+              </div>
+          </div>
       </div>
   )
 }
